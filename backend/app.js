4fcef86b22a46ae7be4b12edfe997de62a38e2cc
Diogo Neto <diogoneto@ipvc.pt>
2025-06-26 16:27:46 +0100

    Trabalho Finalizado

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 const swaggerDocs = require('./docs/swagger');
-=======
->>>>>>> 7bbfe3c9c6c78e4c9b474ad6fb1b7b892021d474
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -15,25 +12,16 @@ app.use(express.json());
 const alunoRoutes = require('./routes/alunoRoutes');
 app.use('/alunos', alunoRoutes);
 
-<<<<<<< HEAD
 // Swagger (vem depois das rotas para documentar)
 swaggerDocs(app);
 
 // Ligação MongoDB
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Ligação à base de dados com sucesso!'))
-  .catch(err => console.log('Erro na ligação à base de dados:', err));
+    .then(() => console.log('Ligação à base de dados com sucesso!'))
+    .catch(err => console.log('Erro na ligação à base de dados:', err));
 
+// Porta do servidor
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Servidor a correr na porta ${PORT}`);
+    console.log(`Servidor a correr na porta ${PORT}`);
 });
-=======
-const PORT = process.env.PORT || 3001;
-
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    app.listen(PORT, () => console.log(`Servidor a correr na porta ${PORT}`));
-  })
-  .catch(err => console.log(err));
->>>>>>> 7bbfe3c9c6c78e4c9b474ad6fb1b7b892021d474
